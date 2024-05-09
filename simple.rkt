95afe2c6fe7959bb2b72e712b7492da161d576e4
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-05-08 21:57:12 -0500

    Added basic type checking

--- simple.rkt
+++ simple.rkt
@@ -11,112 +11,172 @@
 
      ;; Terminals.
      c
-     x
+     p
+
+     ;; Reference acquisition.
+     (& r ω x)
 
      ;; Variable binding.
-     (letvar x @ p : t = e) ;; bind x of type τ to the result of e, place at ρ
+     (letvar x : t = e) ;; bind x of type τ to the result of e, place at ρ
+     (x : t = e)
 
      ;; Region binding.
      (letrgn [r] e)
      
      ;; Structured control flow
-     (do e e ...) ;; sequencing, racket doesnt like ;
+     {e e ...} ;; sequencing, racket doesnt like ;
      ;; (if e e e)   ;; predication
      ;; (while e e)  ;; recursion
      )
 
+  ;; Constants
+  (c ::= i true false ())
+  (i ::= integer)
+  
+  ;; Place expressions.
+  (p ::=
+     x     ;; variable
+     ;; (* p) ;; dereference
+     ;; (. p i) ;; tuple indexing
+     )
+
+  ;; Variables.
+  (x ::= variable-not-otherwise-mentioned)
+
+  ;; Provenance
+  (ρ ::=
+     r ;; concrete provenance
+     ϱ ;; abstract provenance
+     )
+  ;; Concrete provenance variables.
+  (r ::= variable-not-otherwise-mentioned)
+  ;; Abstract provenance variables.
+  (ϱ ::= variable-not-otherwise-mentioned)
+
+
+  ;; Ownership qualifiers.
+  (ω ::= shared unique)
+
+  ;; References.
 
   ;; Types
   (t ::=
-     ()             ;; unit type
-     int            ;; integer type
-     bool           ;; boolean type
+     unit      ;; unit type
+     int       ;; integer type
+     bool      ;; boolean type
+     (& ρ ω t) ;; reference type
+
+     ;; α              ;; type variable
      ;; ((t ...) -> t) ;; function type
      )
-  
-  ;; Regions
-  (p ::= r)
 
-  ;; Ownership qualifiers.
-  (ω ::= shrd uniq)
-
-  ;; Constants
-  (c ::= i b)
-  (i ::= integer)
-  (b ::= true false)
-  
-  ;; Variables.
-  (x ::= variable-not-otherwise-mentioned)
-  ;; Region variables.
-  (r ::= variable-not-otherwise-mentioned)
 
   #:binding-forms
-  (letvar x @ p : t = e #:refers-to x)
+  (letvar x : t = e) #:exports x
   (letrgn [r] e #:refers-to r)
-  
   )
 
 (default-language Simple)
 
 ;; Extended language for the typechecker.
-(define-extended-language Simple-tc Simple
+(define-extended-language Simple+Γ Simple
   (Γ ::= ((x t) ...))
   )
 
-(define-judgment-form Simple-tc
+(define-judgment-form Simple+Γ
   #:mode (⊢ I I O O)
-  #:contract (⊢ Γ e Γ t) ;; Take Γ and e, produce t (type of e) and new Γ
+  #:contract (⊢ Γ e t Γ) ;; Take Γ and e, produce t (type of e) and new Γ
 
-  [-------------- "int"
-   (⊢ Γ i Γ int )]
+  [-------------- "unit"
+   (⊢ Γ () unit Γ)]
+  
+  [-------------- "integer"
+   (⊢ Γ integer int Γ)]
 
-  [--------------- "bool"
-   (⊢ Γ b Γ bool)]
+  [--------------- "bool true"
+   (⊢ Γ true bool Γ)]
+
+  [--------------- "bool false"
+   (⊢ Γ false bool Γ)]
 
   [----------------------- "variable"
-   (⊢ Γ x Γ (lookup Γ x))]
+   (⊢ Γ x (lookup x Γ) Γ)]
 
-  [(⊢ Γ e Γ_e t)
-   ------------------------------------------------ "variable binding"
-   (⊢ Γ (letvar x @ p : t = e) (extend Γ (x t)) t)]
+  [ ;; TODO: the hard part :3
+   ;; Γ(r) = ∅
+   ;; Γ ⊢ω p => { ℓ }
+   ;; Γ ⊢ω p : t
+   (⊢ Γ x t Γ_x)
+   ----------------- "borrow"
+   (⊢ Γ (& r ω x) (& r ω t) Γ)]
 
-  [(⊢ Γ e Γ_e t)
+  [(⊢ Γ e t Γ_e)
+   --------------------------------------------- "variable declaration"
+   (⊢ Γ (letvar x : t = e) unit (extend x t Γ))]
+
+  [(⊢ Γ e t Γ_e)
+   ---------------------- "variable definition"
+   (⊢ Γ (x : t = e) unit Γ)]
+  
+  [(⊢ Γ e t Γ_e)
    ------------------------- "region binding"
-   (⊢ Γ (letrgn [p] e) Γ t)]
+   (⊢ Γ (letrgn [p] e) t Γ)]
+
+  [(⊢ Γ e t Γ_e)
+   -------------- "sequencing end"
+   (⊢ Γ {e} t Γ)]
   
-  [(⊢ Γ e Γ_e t_e)
-   (⊢ Γ_e (do e_rest ...) Γ_rest t_rest)
-   ------------------------------------- "sequencing"
-   (⊢ Γ (do e e_rest ...) Γ_rest t_rest)]
+  [(⊢ Γ e_1 t_1 Γ_1)
+   (⊢ Γ_1 {e_2 e_rest ...} t_rest Γ_rest)
+   -------------------------------------- "sequencing"
+   (⊢ Γ {e_1 e_2 e_rest ...} t_rest Γ)  ]
   
   )
 
-(define-metafunction Simple-tc
-  extend : Γ (x t) -> Γ
-  [(extend ((x_Γ t_Γ) ...) (x t)) ((x t) (x_Γ t_Γ) ...)]
+(define-metafunction Simple+Γ
+  [(same? t_1 t_1) #t]
+  [(same? t_1 t_2) #f]
   )
 
-(define-metafunction Simple-tc
-  lookup : Γ x -> t
-  [(lookup ((x_1 t_1) ... (x t) (x_2 t_2) ...) x)
-   t
-   (side-condition (not (member (term x) (term (x_1 ...)))))]
-  [(lookup any_1 any_2) ,(error 'lookup "not found : ~e" (term x))]
+(define-metafunction Simple+Γ
+  extend : x t Γ -> Γ
+  [(extend x t ((x_Γ t_Γ) ...)) ((x t) (x_Γ t_Γ) ...)]
+  )
+
+(define-metafunction Simple+Γ
+  lookup : x Γ -> t
+  [(lookup x ((x t)     (x_2 t_2) ...)) t]
+  [(lookup x ((x_1 t_1) (x_2 t_2) ...)) (lookup x ((x_2 t_2) ...))]
   )
 
 ;; Tests for the typechecker.
-(test-judgment-holds (⊢ () 1 any int))
-(test-judgment-holds (⊢ () true any bool))
-(test-judgment-holds (⊢ () false any bool))
-(test-judgment-holds (⊢ () (letvar x @ p : int = 1) any int))
-(test-judgment-holds (⊢ () (letvar x @ p : bool = false) any bool))
+(test-judgment-holds (⊢ () 1 int any))
+(test-judgment-holds (⊢ () true bool any))
+(test-judgment-holds (⊢ () false bool any))
+(test-judgment-holds (⊢ () (letvar x : int = 1) unit any))
+(test-judgment-holds (⊢ () (letvar x : bool = false) unit any))
+(test-judgment-holds (⊢ ()
+                        {(letvar x : bool = false)
+                         (letvar y : bool = true)
+                         x}
+                        bool
+                        any))
+(test-judgment-holds (⊢ ()
+                        {(letvar x : int = 0)
+                         (x : int = 1)
+                         x}
+                        int any))
 
 (test-equal
- (judgment-holds (⊢ () (letvar x @ p : int = false) any int))
+ (judgment-holds (⊢ () (letvar x : int = false) int any))
  #false)
 
 (test-equal
- (judgment-holds (⊢ () (letvar x @ p : int = false) any bool))
+ (judgment-holds (⊢ () (letvar x : int = false) bool any))
+ #false)
+
+(test-equal
+ (judgment-holds (⊢ () {(letvar x : int = false) x} int any))
  #false)
 
 (test-results)
